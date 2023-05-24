f75a7cf9272a0022d55f4e7cbde79cf3bcade326
M-Deepak9843 <130582924+M-Deepak9843@users.noreply.github.com>
2023-05-24 21:52:57 +0530

    Add files via upload

---
+++ pythogorean.py
@@ -0,0 +1,9 @@
+import math
+a=int(input("enter the first num"))
+b=int(input("enter the second num"))
+c=int(input("enter the third num"))
+if(a<b) and (b<c):
+    if c**2==(a**2)+(b**2):
+        print(1)
+else:
+    print(0)
---
+++ rotary6.py
@@ -0,0 +1,7 @@
+name=int(input("enter the name:"))
+age=int(input("enter the age"))
+weight=int(input("enter the weight"))
+if(age>18) and (weight>40):
+    print("eligible")
+else:
+    print ("not eligible")
